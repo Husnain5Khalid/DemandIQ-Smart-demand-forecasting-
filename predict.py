bfd34401d01c907414d9654cb7f7845ff7971cd1
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-07-27 18:08:40 +0300

    Predict.py created

---
+++ predict.py
@@ -0,0 +1,16 @@
+from src.components.data_ingestion import DataIngestion
+from src.pipelines.prediction_pipeline import PredictionPipeline
+
+ingestion = DataIngestion()
+
+_, valid_df = ingestion.load_data()
+
+sample = valid_df.drop(
+    columns=["sales"]
+).head()
+
+pipeline = PredictionPipeline()
+
+prediction = pipeline.predict(sample)
+
+print(prediction)
