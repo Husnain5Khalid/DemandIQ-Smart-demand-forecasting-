8c1661997eeb5b7c1b7e1ff3ba12f1475b56ee9f
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-07-27 18:00:47 +0300

    main file created

---
+++ main.py
@@ -0,0 +1,11 @@
+from demand_iq.pipelines.training_pipeline import TrainingPipeline
+
+if __name__ == "__main__":
+
+    pipeline = TrainingPipeline()
+
+    model, metrics = pipeline.run_pipeline()
+
+    print(metrics)
+
+    
